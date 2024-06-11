fc807ba6e1f0713e2f54ab32f7a39902a2be6883
honginSon <sonhi2000@naver.com>
2024-06-11 15:14:49 +0900

    프로젝트 생성

---
+++ src/test/java/com/hongin/webservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongin.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
