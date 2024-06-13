da92f0e4606950f428c79b34777f298f9a849000
honginSon <sonhi2000@naver.com>
2024-06-13 14:54:03 +0900

    API 생성

--- src/main/java/com/hongin/webservice/Application.java
+++ src/main/java/com/hongin/webservice/Application.java
@@ -2,7 +2,9 @@ package com.hongin.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
---
+++ src/test/java/com/hongin/webservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.hongin.webservice.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanUp() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() throws Exception {
+        //given
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("sonhi2000@naver.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo("테스트 게시글");
+        assertThat(posts.getContent()).isEqualTo("테스트 본문");
+    }
+    
+    @Test
+    public void BaseTimeEntity_등록() throws Exception {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("sonhi2000@naver.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreatedDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
+    }
+}
