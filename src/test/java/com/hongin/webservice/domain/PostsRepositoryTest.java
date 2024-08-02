9557ae6465fc9e8be704cc7e4b769e9daa2383be
honginSon <92095858+honginSon@users.noreply.github.com>
2024-08-03 01:40:49 +0900

    [2] 간단 API 만들기

---
+++ src/test/java/com/hongin/webservice/domain/PostsRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.hongin.webservice.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired PostsRepository postsRepository;
+
+    @Test
+    public void 게시글저장_불러오기() throws Exception {
+        //given
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("sonhi2000@naver.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts post = postsList.getFirst();
+        assertThat(post.getTitle()).isEqualTo("테스트 게시글");
+        assertThat(post.getContent()).isEqualTo("테스트 본문");
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() throws Exception {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("sonhi2000@naver.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.getFirst();
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
