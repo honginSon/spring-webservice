8be0c24e62e5059face38d209c5cbd9743b38b98
honginSon <sonhi2000@naver.com>
2024-08-15 12:17:53 +0900

    Revert "set0, set2 profile 설정"
    This reverts commit 748d094106cca36d77ce00d041c17eced36f8b6b.

--- src/main/java/com/hongin/webservice/web/WebRestController.java
+++ src/main/java/com/hongin/webservice/web/WebRestController.java
@@ -3,20 +3,16 @@ package com.hongin.webservice.web;
 import com.hongin.webservice.dto.PostsSaveRequestDto;
 import com.hongin.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     private PostsService postsService;
-    private Environment env;
 
     @GetMapping("/hello")
     public String hello() {
@@ -26,12 +22,5 @@ public class WebRestController {
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto) {
         postsService.save(dto);
-    }
-
-    @GetMapping("/profile")
-    public String getProfile() {
-        return Arrays.stream(env.getActiveProfiles())
-                .findFirst()
-                .orElse("");
     }
 }
